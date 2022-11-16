9b325d2a5d7faf4c4044eed4e129bcf2bebe8d7a
User <User@F602-60>
2022-11-16 11:35:29 +0800

    下拉式選單

--- CsharpReport/editForm.cs
+++ CsharpReport/editForm.cs
@@ -6,11 +6,14 @@ using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static CsharpReport.Form1;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using ComboBox = System.Windows.Forms.ComboBox;
 
 namespace CsharpReport
 {
@@ -19,6 +22,7 @@ namespace CsharpReport
         public editForm()
         {
             InitializeComponent();
+            GetBookData();
         }
 
         private void Load_DB()
@@ -27,6 +31,74 @@ namespace CsharpReport
             DBConfig.sqlite_connect.Open();// Open
         }
 
+        /// <summary>
+        /// 下拉式選單的內容
+        /// </summary>
+        private void GetBookData()
+        {
+            
+            var command = DBConfig.sqlite_connect.CreateCommand();
+            string sql = @"SELECT member_id, member_name
+                            FROM member";
+            string status = "";
+
+            command.CommandText = sql;
+            DBConfig.sqlite_datareader = command.ExecuteReader();
+
+            if (DBConfig.sqlite_datareader.HasRows)
+            {
+                while (DBConfig.sqlite_datareader.Read()) //read every data
+                {
+                    string member_id = Convert.ToString(DBConfig.sqlite_datareader["member_id"]);
+                    string member_name = Convert.ToString(DBConfig.sqlite_datareader["member_name"]);
+
+                    comboBox1.Items.Add(new ComboBoxItem(member_id, member_name));
+                }
+                //DBConfig.sqlite_datareader.Close();
+            }
+        }
+        /// <summary>
+        /// 設定下拉式選單的Value與Text
+        /// </summary>
+        public class ComboBoxItem
+        {
+            public string Value { get; set; }
+            public string Text { get; set; }
+            public ComboBoxItem(string value, string text)
+            {
+                Value = value;
+                Text = text;
+            }
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+        /// <summary>
+        /// 取得下拉式選單的Value
+        /// </summary>
+        public class ComboUtil
+        {
+            /// <summary>
+            /// 取得下拉項目的值
+            /// </summary>
+            /// <param name="cbo">物件</param>
+            /// <returns></returns>
+            public static ComboBoxItem GetItem(ComboBox cbo)
+            {
+                ComboBoxItem item = new ComboBoxItem("", "");
+                if (cbo.SelectedIndex > -1)
+                {
+                    item = cbo.Items[cbo.SelectedIndex] as ComboBoxItem;
+                }
+                return item;
+            }
+
+        }
+
+        /// <summary>
+        /// 設定目前的書籍資料
+        /// </summary>
         public object[] setValue
         {
             set
@@ -56,7 +128,9 @@ namespace CsharpReport
         /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            //取得下拉式選單的值
+            //ComboUtil.GetItem(comboBox1).Value
+
             var command = DBConfig.sqlite_connect.CreateCommand();
             command.CommandText = @"UPDATE book_data
                         SET book_name = @book_name,writer = @writer,publish = @publish,
@@ -117,5 +191,6 @@ namespace CsharpReport
             }
             
         }
+
     }
 }
