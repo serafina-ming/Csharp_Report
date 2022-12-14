bdbc056e21a02f5be216974bde3de6625ec4271f
User <User@F602-60>
2022-12-14 11:52:40 +0800

    匯出pdf表格

--- CsharpReport/Form1.cs
+++ CsharpReport/Form1.cs
@@ -8,6 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using iText.Forms;
+using iText.IO.Font;
+using iText.IO.Image;
+using iText.Kernel.Colors;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf.Extgstate;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using iText.Kernel.Pdf.Canvas.Draw;
+using TabAlignment = iText.Layout.Properties.TabAlignment;
+using iText.Layout.Borders;
 
 namespace CsharpReport
 {
@@ -252,5 +267,164 @@ namespace CsharpReport
             exportOrImportForm = new exportOrImportForm();
             exportOrImportForm.ShowDialog();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            PrintPDF();
+        }
+
+        void PrintPDF()
+        {
+            // Set the output dir and file name
+            // string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            string src = "./test.pdf";
+            string dst = @"./new_test.pdf";
+
+            //manipulatePdf(src, dst);
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); ;
+            saveFileDialog.FileName = "書籍資料";
+            saveFileDialog.Filter = "pdf files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                dst = saveFileDialog.FileName;
+                manipulatePdf(src, dst);
+            }
+
+        }
+
+        void manipulatePdf(String src, String dst)
+        {
+            // 1. create pdf
+            PdfWriter writer = new PdfWriter(dst);
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+
+            // 標楷體
+            PdfFont font_tr = PdfFontFactory.CreateFont(@"c:/Windows/fonts/kaiu.ttf", PdfEncodings.IDENTITY_H);
+            // 正黑體
+            //PdfFont font_msjh = PdfFontFactory.CreateFont(@"./msjh.ttf", PdfEncodings.IDENTITY_H);
+            document.SetFont(font_tr); // 預設中文字型
+            
+            // 2. 加文字
+
+            // 2.1. add header
+            Paragraph header_1 = new Paragraph("書籍資料")
+               .SetTextAlignment(TextAlignment.CENTER)
+               .SetFontSize(36)
+               .SetFont(font_tr);
+            document.Add(header_1);
+
+            //Paragraph header_2 = new Paragraph("文件 標題2")
+            //   .SetTextAlignment(TextAlignment.CENTER)
+            //   .SetFontSize(24);
+            //document.Add(header_2);
+
+            // Line separator
+            LineSeparator ls = new LineSeparator(new SolidLine());
+            document.Add(ls);
+
+
+
+            float[] colWidths = { 1, 6, 3, 2, 2, 2, 2};
+            // Creating a table       
+            Table table = new Table(UnitValue.CreatePercentArray(colWidths));
+
+            // Add paragraph 1
+            //增加段落一
+            Paragraph paragraph = new Paragraph();
+
+            //TabStop TabStop1 = new TabStop(50f);
+            //paragraph.AddTabStops(TabStop1);
+            //Tab Tab1 = new Tab();
+            //TabStop TabStop2 = new TabStop(250f);
+            //paragraph.AddTabStops(TabStop2);
+            //Tab Tab2 = new Tab();
+            //讀取grid資料
+            String Gridparagraph = "";
+            //Gridparagraph = Gridparagraph + "書籍編號\t書名\t作者\t出版社\t類型\t借閱狀態\t借閱人\n";
+
+            //paragraph.Add("編號").Add(Tab1).Add("書名").Add(Tab2).Add("作者").Add(Tab1).Add("出版社").Add(Tab1).Add("類型").Add(Tab1).Add("借閱狀態").Add(Tab1).Add("借閱人\n");
+
+ 
+            table.AddCell("編號");
+            table.AddCell("書名");
+            table.AddCell("作者");
+            table.AddCell("出版社");
+            table.AddCell("類型");
+            table.AddCell("借閱狀態");
+            table.AddCell("借閱人");
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j <= 6; j++)
+                {
+                    //Gridparagraph = Gridparagraph + Convert.ToString(dataGridView1.Rows[i].Cells[j].Value)+"\t";
+                    Gridparagraph = Convert.ToString(dataGridView1.Rows[i].Cells[j].Value);
+                    // Adding cells to the table       
+                    Cell cell = new Cell().Add(new Paragraph(Gridparagraph));
+                    cell.SetBorder(new SolidBorder(ColorConstants.WHITE, 0));
+                    cell.SetBorderBottom(new SolidBorder(ColorConstants.GRAY, 0));
+                    //table.addCell(cell);
+                    table.AddCell(cell);
+                    //if (j == 1)
+                    //{
+                    //    paragraph.Add(Gridparagraph).Add(Tab1);
+                    //}
+                    //else 
+                    //{ 
+                    //    paragraph.Add(Gridparagraph).Add(Tab2);
+                    //}
+                }
+                //Gridparagraph = Gridparagraph + "\n";
+                //paragraph.Add("\n");
+            }
+            //paragraph.Add("111");
+            //paragraph.AddTabStops(new TabStop(100f, TabAlignment.LEFT));
+            //paragraph.Add(new Tab());
+            //paragraph.Add(Gridparagraph);
+
+            
+
+            // Adding Table to document        
+            document.Add(table);
+
+            //document.Add(paragraph);
+
+            // Add paragraph 2
+            //設定在右下角
+            paragraph = new Paragraph("根據《今日美國報》報導，美職28日賽後確定了16" +
+                "支季後賽球隊的對戰席次，戰況膠著的國聯因巨人、費城人都輸球，釀酒人搶" +
+                "下最後一張晉級門票，他們將作客對上頭號種子道奇，國聯第2種子勇士將迎戰" +
+                "紅人，第3種子小熊將迎戰今年鹹魚大翻身的馬林魚，4、5種子教士與紅雀正面" +
+                "對決。");
+            paragraph.SetMarginLeft(123);
+            paragraph.SetMarginTop(555);
+            document.Add(paragraph);
+
+            //// 隨便插一段話
+            //PdfPage page1 = pdf.GetPage(4); // ##設定文字方塊插在哪一頁
+            //PdfCanvas pdfCanvas1 = new PdfCanvas(page1);
+            //iText.Kernel.Geom.Rectangle rectangle = new iText.Kernel.Geom.Rectangle(100, 700, 100, 100); // ##設定文字方塊的位置與大小
+            //iText.Kernel.Colors.Color bgColour = new DeviceRgb(255, 504, 204);  // ##設定文字方塊的顏色
+            //pdfCanvas1.SaveState()
+            //        .SetFillColor(bgColour)
+            //        .Rectangle(rectangle)
+            //        .Fill()
+            //        .RestoreState();
+            ////iText.Layout.Canvas canvas = new iText.Layout.Canvas(pdfCanvas1, pdf, rectangle);
+            //iText.Layout.Canvas canvas = new iText.Layout.Canvas(pdfCanvas1, rectangle);
+            //canvas.Add(new Paragraph("隨便插一段話").SetFont(font_tr));  // ##設定文字方塊的文字
+
+            // 4. close content
+            //canvas.Close();
+            document.Close();
+        }
+
     }
 }
